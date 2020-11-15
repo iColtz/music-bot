f309a620b2d2e304c9af8196bba31fe87f12316d
iColtz <max.hurst@hotmail.com>
2020-11-15 15:52:56 +0000

    command: queue command

---
+++ src/commands/queue.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord.js-akago');
+
+class QueueCommand extends Command {
+    constructor() {
+        super('queue', {
+            description: 'Gives a list of the current playing queue',
+            category: 'Music',
+            aliases: ['q'],
+            guildOnly: true,
+            cooldown: 5,
+        });
+    }
+
+    async execute(message) {
+        const { guild, channel } = message;
+        const serverQueue = this.client.queue.get(guild.id);
+        if (!serverQueue) return channel.send('There is currently no queue in this guild.');
+        let i = 0;
+        return message.channel.send([
+            '__**Music Queue**__',
+            serverQueue.songs.map(song => `**${++i}.** ${song.title}`).join('\n'),
+        ].join('\n'));
+    }
+}
+
+module.exports = QueueCommand;
