aa6bf17dc578effbe766f74164617bf0884c95e9
iColtz <max.hurst@hotmail.com>
2020-11-15 22:48:23 +0000

    commands: loop command

---
+++ src/commands/loop.js
@@ -0,0 +1,23 @@
+const { Command } = require('discord.js-akago');
+
+class LoopCommand extends Command {
+    constructor() {
+        super('loop', {
+            description: 'Loop the current queue or song.',
+            category: 'Music',
+            aliases: ['l'],
+            guildOnly: true,
+            cooldown: 0,
+        });
+    }
+
+    async execute(message) {
+        const { guild, channel } = message;
+        const serverQueue = this.client.queue.get(guild.id);
+        if (!serverQueue) return channel.send('There is currently no queue in this guild.');
+        serverQueue.loop = !serverQueue.loop;
+        channel.send(serverQueue.loop ? 'Enabled queue loop.' : 'Disabled queue loop.');
+    }
+}
+
+module.exports = LoopCommand;
