a5d12da4d136bf6836d942dfd9be529426735338
iColtz <max.hurst@hotmail.com>
2020-11-15 19:39:43 +0000

    commands: resume command

---
+++ src/commands/resume.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord.js-akago');
+
+class ResumeCommand extends Command {
+    constructor() {
+        super('resume', {
+            description: 'Resumes the current playing song.',
+            category: 'Music',
+            aliases: ['rs'],
+            guildOnly: true,
+            cooldown: 5,
+        });
+    }
+
+    async execute(message) {
+        const { guild, channel } = message;
+        const serverQueue = this.client.queue.get(guild.id);
+        if (!serverQueue) return channel.send('There is currently no queue in this guild.');
+        if (serverQueue.playing) return channel.send('The music is already playing.');
+        serverQueue.playing = true;
+        serverQueue.connection.dispatcher.resume();
+    }
+}
+
+module.exports = ResumeCommand;
