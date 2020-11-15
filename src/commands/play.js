dac974537cd2b80001dd332328f197b8277d1a5f
iColtz <max.hurst@hotmail.com>
2020-11-15 14:44:30 +0000

    command: added queue system to play command

--- src/commands/play.js
+++ src/commands/play.js
@@ -13,21 +13,61 @@ class PlayCommand extends Command {
     }
 
     async execute(message, [url]) {
-        const { voice } = message.member;
+        const { voice, guild } = message.member;
+        let serverQueue = this.client.queue.get(guild.id);
         if (!voice.channel) return message.channel.send('You need to be in a voice channel to play music.');
-        if (!url) return message.channel.send('You need to specific a youtube link to play.'); 
+        if (!url) return message.channel.send('You need to specific a youtube link to play.');
 
-        try {
-            const connection = await voice.channel.join();
-            const stream = ytdl(url, { filter: 'audioonly' });
-            var dispatcher = connection.play(stream, { volume: 0.5 }); // eslint-disable-line no-var
-        }
-        catch (error) {
-            console.log(error);
-            return message.channel.send('There was an error while trying to play a song.');
-        }
+        const play = async (song) => {
+            serverQueue = this.client.queue.get(guild.id);
 
-        dispatcher.on('finish', () => voice.channel.leave());
+            if (!song) {
+                serverQueue.voiceChannel.leave();
+                this.client.queue.delete(guild.id);
+                return;
+            }
+
+            try {
+                const connection = await voice.channel.join();
+                const stream = ytdl(song.url, { filter: 'audioonly' });
+                var dispatcher = connection.play(stream, { volume: 0.5 }); // eslint-disable-line no-var
+                serverQueue.connection = connection;
+            }
+            catch (error) {
+                console.log(error);
+                return message.channel.send('There was an error while trying to play a song.');
+            }
+    
+            message.channel.send(`Started Playing: **${song.title}**`);
+            dispatcher.on('finish', () => {
+                serverQueue.songs.shift();
+                play(serverQueue.songs[0]);
+            });
+        };
+
+        const songInfo = await ytdl.getBasicInfo(url);
+        if (!songInfo) return message.channel.send('No results found for this url.');
+
+        const song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+        };
+
+        if (!serverQueue) {
+            const queueConstruct = {
+                guild: guild,
+                voiceChannel: voice.channel,
+                songs: [song],
+                connection: null,
+            };
+
+            this.client.queue.set(guild.id, queueConstruct);
+            play(queueConstruct.songs[0]);
+        }
+        else {
+            serverQueue.songs.push(song);
+            message.channel.send(`Added **${song.title}** to the queue.`);
+        }
     }   
 }
 
