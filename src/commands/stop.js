c73931bb7577aee6980f4a6147ca3ee77f308e51
iColtz <max.hurst@hotmail.com>
2020-11-16 19:09:10 +0000

    commands: sending a message when music is stopped

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -12,12 +12,13 @@ class StopCommand extends Command {
     async execute(message) {
         const { voice, guild } = message.guild.me;
         const serverQueue = this.client.queue.get(guild.id);
-        if (!serverQueue) return message.channel.send('🛑 There is no queue in this guild.');
+        if (!serverQueue) return message.channel.send('There is no queue in this guild.');
         if (!this.client.util.canModifyQueue(message)) return;
 
         try {
             this.client.queue.delete(guild.id);
             voice.channel.leave();
+            message.channel.send('🛑 Stopped the music.');
         }
         catch (error) {
             console.log(error);
